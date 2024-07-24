c6e65d959f0c5f16bbaad592d390ec897d207c76
Human <vladislav.rogovoy@gmail.com>
2024-07-24 16:03:49 -0400

    move sensitive values from config.yml to CircleCi Environment Vars

--- src/main/java/com/cydeo/fakeuserapp/controller/Welcome.java
+++ src/main/java/com/cydeo/fakeuserapp/controller/Welcome.java
@@ -8,7 +8,8 @@ public class Welcome {
     @GetMapping
     public String welcome(){
         StringBuilder httpResponse = new StringBuilder();
-        httpResponse.append("<html><body> <h1>Welcome to fake user app July 24</h1>");
+        httpResponse.append("<html><body> <h1>Welcome to fake user app </h1>");
+        httpResponse.append("<br><h3>move sensitive info from config.yml to CircleCi > Project Settings > Environment Variables</h3>");
         httpResponse.append("<a href='/api/v1/no-db/random'>no-db page</a><br>");
         httpResponse.append("<a href='/api/v1/with-db/random'>with-db page</a><br>");
         httpResponse.append("</body></html>");
